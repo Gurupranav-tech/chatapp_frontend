bac4ab28311bcfb405034abd01b80c5b54476402
Gurupranav-tech <gurupranav08@gmail.com>
2025-01-26 05:39:42 +0000

    Added support for emojis

--- src/components/SendMessags.tsx
+++ src/components/SendMessags.tsx
@@ -2,13 +2,19 @@ import { GoPaperclip, GoSmiley } from "react-icons/go";
 import { FaMicrophone } from "react-icons/fa";
 import { IoMdSend } from "react-icons/io";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useConversations } from "../contexts/ConversationsContext";
 import { USERNAME } from "../utils/generate_dummy_data";
+import EmojiPicker from "emoji-picker-react";
 
 export default function SendMessage() {
   const [message, setMessage] = useState("");
+  const [show, setShow] = useState(false);
   const { addPersonalChat, selected, addGroupChat } = useConversations();
+
+  useEffect(() => {
+    document.addEventListener("click", () => setShow(false));
+  }, []);
 
   const handleMainBtn = () => {
     if (!message) return;
@@ -20,9 +26,24 @@ export default function SendMessage() {
 
   return (
     <div className="flex items-center gap-5">
-      <div className="bg-[#EFF6FC] flex-1 text-lg py-2 px-4 rounded-xl flex gap-3 items-center">
+      <div className="relative bg-[#EFF6FC] flex-1 text-lg py-2 px-4 rounded-xl flex gap-3 items-center">
         <GoPaperclip className="outline-none text-gray-700 text-2xl cursor-pointer" />
-        <GoSmiley className="outline-none text-gray-700 text-2xl cursor-pointer" />
+        <GoSmiley
+          className="outline-none text-gray-700 text-2xl cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            setShow(true);
+          }}
+        />
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={`emoji-picker-container ${!show ? "hidden" : "block"}`}
+        >
+          <EmojiPicker
+            open={show}
+            onEmojiClick={(e) => setMessage((m) => m + e.emoji)}
+          />
+        </div>
         <input
           placeholder="Type your message here..."
           className="outline-none w-full bg-transparent text-gray-700"
